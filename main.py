e9ccd45e5123d0c97f1636eb3ea0ef4aa92f2c73
beatriz-rios <beatriz.rios6@aluno.senai.br>
2026-02-23 17:22:25 -0300

    ...

---
+++ main.py
@@ -0,0 +1,13 @@
+import flet as ft
+#importa a biblioteca flet e cria um apelido (alias)
+
+def main(page: ft.Page):
+    page.title = "Meu primeiro App Flet" #Define o título da janela
+    page.bgcolor = "purple" #Define a cor de fundo da janela
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER #Alinha o conteúdo verticalmente ao centro    
+    page.horizontal_alignment = ft.MainAxisAlignment.CENTER #Alinha o conteúdo horizontalmente ao centro    
+    page.add(
+        ft.Text("Bem vindo ao meu App!"),
+        ft.Text("Aqui você pode criar o que quiser")
+    )
+ft.run(main)
