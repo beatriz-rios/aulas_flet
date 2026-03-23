aaddbb10be72057cf43bc3edfa18e51a91a0c252
beatriz-rios <beatriz.rios6@aluno.senai.br>
2026-03-23 15:34:06 -0300

    ...

--- atividadeMiau.py
+++ atividadeMiau.py
@@ -3,15 +3,16 @@ import flet as ft
 def main(page: ft.Page):
     def mostrar(e):
         page.add(
-            ft.Text("Miau!prrrrrr...")
+            ft.Text("Miau!prrrrrr..."),
+            ft.Image(
+                src='images/gato.jpg',
+                height=120
+            )
         )
 
     page.add(
             ft.Text('Eu sou um gato muito fofo!'),
-            ft.Image(
-                src='images/gato.jpg',
-                height=120
-            ),
+            
             ft.Button(
                 content='Clique aqui para eu ronronar!',
                 on_click=mostrar
