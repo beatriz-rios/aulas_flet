167bbf41b80586110f59a6d43b51417893189c8d
beatriz-rios <beatriz.rios6@aluno.senai.br>
2026-03-23 14:59:56 -0300

    atividadeMiau.py

---
+++ atividadeMiau.py
@@ -0,0 +1,21 @@
+import flet as ft
+
+def main(page: ft.Page):
+    def mostrar(e):
+        page.add(
+            ft.Text("Miau!prrrrrr...")
+        )
+
+    page.add(
+            ft.Text('Eu sou um gato muito fofo!'),
+            ft.Image(
+                src='images/gato.jpg',
+                height=120
+            ),
+            ft.Button(
+                content='Clique aqui para eu ronronar!',
+                on_click=mostrar
+            )
+        )
+
+ft.run(main)
--- onepiece.py
+++ onepiece.py
@@ -1,19 +1,19 @@
-import flet as ft
+import flet as ft #importando bibioteca e apelido(as)
 
-def main(page: ft.Page):
-    def mostrar_mensagem(e):
-        page.add(
+def main(page: ft.Page):#def é definir a função
+    def mostrar_mensagem(e):# (e) o "e" é um parametro de evento
+        page.add(# page é a pagina e .add é adicionar um elemento a page 
             ft.Text("Eu vou ser o rei dos piratas!")
         )
-    page.add(ft.Text("Olá meu nome é Luffy"),
-             ft.Image(
+    page.add(ft.Text("Olá meu nome é Luffy"),#ft.Text é uma component
+             ft.Image(#para ter componentes de images nescessita de uma pasta assets para documentação
                  src='images/onepiece1.jpg',
                  height=150
              ),
              ft.Button(
-                 content = 'Clique aqui',
-                 on_click=mostrar_mensagem
-             )
+                 content = 'Clique aqui',#defifnindo o conteudo do botao
+                 on_click=mostrar_mensagem #dando função para o evento do botão
+             )#resumindo: cria oq aparece no site. e depois de uma função para o evento chamando ela 
              
     )    
 
